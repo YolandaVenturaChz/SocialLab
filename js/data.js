3aac2ab61f9fd5b2c28a9bf1ddc432a396e808b0
DDCenteno <danielacentenoramos@gmail.com>
2018-01-07 09:42:36 -0500

    agregando

--- js/data.js
+++ js/data.js
@@ -1,85 +1,85 @@
 var data=[ estudent = [
   {
     name: 'Juana Gonzales Luna',
-    address: "Peru",
-    cel:'999999999',
-    email:'juanita@gmail.com',
-    filters: ["Front End Developer, UX Designer, UI Designer"],
-    photo: "<img src='assets/img/.png' alt='' class='mouseover'>"
+    address: 'Peru',
+    cel: '999999999',
+    email: 'juanita@gmail.com',
+    filters: ['Front End Developer, UX Designer, UI Designer'],
+    photo: '<img src="assets/img/.png" alt="" class="mouseover">'
   },
   {
     name: 'Elizabeth Humberto Rito',
-    address: "Mexico",
+    address: 'Mexico',
     cel: '999999999',
     email: 'juanita@gmail.com',
-    filters: [" UX Designer"],
-    photo: "<img src='assets/img/.png' alt='' class='mouseover'>"
+    filters: [' UX Designer'],
+    photo: '<img src="assets/img/.png" alt="" class="mouseover">'
   },
   {
     name: 'Rafaela Remo del Castillo',
-    address: "Brasil",
+    address: 'Brasil',
     cel: '999999999',
     email: 'juanita@gmail.com',
-    filters: ["Back End Developer"],
-    photo: "<img src='assets/img/.png' alt='' class='mouseover'>"
+    filters: ['Back End Developer'],
+    photo: '<img src="assets/img/.png" alt="" class="mouseover">'
   },
   {
     name: 'Dayce Ventura Buendia',
-    address: "Chile",
+    address: 'Chile',
     cel: '999999999',
     email: 'juanita@gmail.com',
-    filters: ["Front End Developer, Back End Developer"],
-    photo: "<img src='assets/img/.png' alt='' class='mouseover'>"
+    filters: ['Front End Developer, Back End Developer'],
+    photo: '<img src="assets/img/.png" alt="" class="mouseover">'
   },
   {
     name: 'Alejandra Segura Calvin',
-    address: ["Peru"],
+    address: ['Peru'],
     cel: '999999999',
     email: 'juanita@gmail.com',
-    filters: ["Front End Developer, Back End Developer, UX Designer, UI Designer"],
-    photo: "<img src='assets/img/.png' alt='' class='mouseover'>"
+    filters: ['Front End Developer, Back End Developer, UX Designer, UI Designer'],
+    photo: '<img src="assets/img/.png" alt="" class="mouseover">'
   },
 ],
- mentores = [
-    {
-      name: 'Pepito Bodoque',
-      address: ["Peru"],
-      cel: '999999999',
-      email: 'juanita@gmail.com',
-      filters: ["Front End Developer, Back End Developer, UX Designer, UI Designer"],
-      photo: "<img src='assets/img/.png' alt='' class='mouseover'>"
-    },
-    {
-      name: 'Lulú Palacios',
-      address: ["Chile"],
-      cel: '999999999',
-      email: 'juanita@gmail.com',
-      filters: ["Front End Developer, Back End Developer, UX Designer, UI Designer"],
-      photo: "<img src='assets/img/.png' alt='' class='mouseover'>"
-    },
-    {
-      name: 'Lalo Gomez',
-      address: ["mexico"],
-      cel: '999999999',
-      email: 'juanita@gmail.com',
-      filters: ["UX Designer"],
-      photo: "<img src='assets/img/.png' alt='' class='mouseover'>"
-    },
-    {
-      name: 'Ruth Estrada',
-      address: ["mexico"],
-      cel: '999999999',
-      email: 'juanita@gmail.com',
-      filters: ["Front End Developer, Back End Developer"],
-      photo: "<img src='assets/img/.png' alt='' class='mouseover'>"
-    },
-    {
-      name: 'Joalbert Puma',
-      address: ["chile"],
-      cel: '999999999',
-      email: 'juanita@gmail.com',
-      filters: ["Front End Developer, Back End Developer"],
-      photo: "<img src='assets/img/.png' alt='' class='mouseover'>"
-    },
-  ]
+mentores = [
+  {
+    name: 'Pepito Bodoque',
+    address: ['Peru'],
+    cel: '999999999',
+    email: 'juanita@gmail.com',
+    filters: ['Front End Developer, Back End Developer, UX Designer, UI Designer'],
+    photo: '<img src="assets/img/.png" alt="" class="mouseover">'
+  },
+  {
+    name: 'Lulú Palacios',
+    address: ['Chile'],
+    cel: '999999999',
+    email: 'juanita@gmail.com',
+    filters: ['Front End Developer, Back End Developer, UX Designer, UI Designer'],
+    photo: '<img src="assets/img/.png" alt="" class="mouseover">'
+  },
+  {
+    name: 'Lalo Gomez',
+    address: ['mexico'],
+    cel: '999999999',
+    email: 'juanita@gmail.com',
+    filters: ['UX Designer'],
+    photo: '<img src="assets/img/.png" alt="" class="mouseover">'
+  },
+  {
+    name: 'Ruth Estrada',
+    address: ['mexico'],
+    cel: '999999999',
+    email: 'juanita@gmail.com',
+    filters: ['Front End Developer, Back End Developer'],
+    photo: '<img src="assets/img/.png" alt="" class="mouseover">'
+  },
+  {
+    name: 'Joalbert Puma',
+    address: ['chile'],
+    cel: '999999999',
+    email: 'juanita@gmail.com',
+    filters: ['Front End Developer, Back End Developer'],
+    photo: '<img src="assets/img/.png" alt="" class="mouseover">'
+  },
 ]
+];
