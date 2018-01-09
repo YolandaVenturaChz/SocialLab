f3028711557b3b69d08e8e91dd5a1b832764005a
yoli21 <y_venturchz@hotmail.com>
2018-01-09 00:38:44 -0500

    avances

---
+++ js/datafriend.js
@@ -0,0 +1,150 @@
+
+var data = {
+    'gossip': {
+        'characters': [
+            {
+                'name': 'Blair Waldorf',
+                'picture': '../assets/images/gossip/blair.jpg',
+                'school': 'Constance Billard School',
+                'city': 'Manhattan, New York'
+            },
+            {
+                'name': 'Serena Van Der Woodsen',
+                'picture': '../assets/images/gossip/serena.jpg',
+                'school': 'Constance Billard School',
+                'city': 'Manhattan, New York'
+            },
+            {
+                'name': 'Jenny Humphrey',
+                'picture': '../assets/images/gossip/jenny.jpg',
+                'school': 'Constance Billard School',
+                'city': 'Brooklyn, New York'
+            },
+            {
+                'name': 'Chuck Bass',
+                'picture': '../assets/images/gossip/chuck.jpg',
+                'school': 'Riverside Preparatory School',
+                'city': 'Manhattan, New York'
+            },
+            {
+                'name': 'Nate Archibald',
+                'picture': '../assets/images/gossip/nate.jpg',
+                'school': 'Riverside Preparatory School',
+                'city': 'Manhattan, New York'
+            },
+            {
+                'name': 'Dan Humphrey',
+                'picture': '../assets/images/gossip/dan.jpg',
+                'school': 'Riverside Preparatory School',
+                'city': 'Brooklyn, New York'
+            }
+        ]
+    },
+    'breakingbad': {
+        'characters': [
+            {
+                'name': 'Walter White',
+                'picture': '../assets/images/breakingbad/walter.jpg',
+                'city': 'Albuquerque'
+            },
+            {
+                'name': 'Skyler White',
+                'picture': '../assets/images/breakingbad/skyler.jpg',
+                'city': 'Albuquerque'
+            },
+            {
+                'name': 'Jesse Pinkman',
+                'picture': '../assets/images/breakingbad/jesse.jpg',
+                'city': 'Albuquerque'
+            },
+            {
+                'name': 'Gus Fring',
+                'picture': '../assets/images/breakingbad/gus.jpg',
+                'city': 'Albuquerque'
+            },
+            {
+                'name': 'Mike Ehrmantraut',
+                'picture': '../assets/images/breakingbad/mike.jpg',
+                'city': 'Albuquerque'
+            }
+        ]
+    },
+    'howtoget': {
+        'characters': [
+            {
+                'name': 'Annalise Keating',
+                'picture': '../assets/images/howtoget/annalise.jpg',
+                'city': 'Philadelphia'
+            },
+            {
+                'name': 'Connor Walsh',
+                'picture': '../assets/images/howtoget/connor.png',
+                'city': 'Philadelphia'
+            },
+            {
+                'name': 'Wes Gibbins',
+                'picture': '../assets/images/howtoget/wes.jpg',
+                'city': 'Philadelphia'
+            },
+            {
+                'name': 'Asher Millstone',
+                'picture': '../assets/images/howtoget/asher.jpg',
+                'city': 'Philadelphia'
+            },
+            {
+                'name': 'Michaela Pratt',
+                'picture': '../assets/images/howtoget/michaela.jpg',
+                'city': 'Philadelphia'
+            },
+            {
+                'name': 'Frank Delfino',
+                'picture': '../assets/images/howtoget/frank.jpg',
+                'city': 'Philadelphia'
+            },
+            {
+                'name': 'Bonnie Winterbottom',
+                'picture': '../assets/images/howtoget/bonnie.jpg',
+                'city': 'Philadelphia'
+            }
+        ]
+    },
+    'stranger': {
+        'characters': [
+            {
+                'name': 'Eleven',
+                'picture': '../assets/images/stranger/eleven.jpg',
+                'city': 'Hawkins, Indiana'
+            },
+            {
+                'name': 'Mike Wheeler',
+                'picture': '../assets/images/stranger/mike.jpg',
+                'city': 'Hawkins, Indiana'
+            },
+            {
+                'name': 'Dustin Henderson',
+                'picture': '../assets/images/stranger/dustin.jpg',
+                'city': 'Hawkins, Indiana'
+            },
+            {
+                'name': 'Lucas Sinclair',
+                'picture': '../assets/images/stranger/lucas.jpg',
+                'city': 'Hawkins, Indiana'
+            },
+            {
+                'name': 'Will Byers',
+                'picture': '../assets/images/stranger/will.jpg',
+                'city': 'Hawkins, Indiana'
+            },
+            {
+                'name': 'Nancy Wheeler',
+                'picture': '../assets/images/stranger/nancy.png',
+                'city': 'Hawkins, Indiana'
+            },
+            {
+                'name': 'Joyce Byers',
+                'picture': '../assets/images/stranger/joyce.jpg',
+                'city': 'Hawkins, Indiana'
+            }
+        ]
+    }
+}
