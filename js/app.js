e67681422d677dcaf9ae2caf986b4f2488e39de3
DDCenteno <danielacentenoramos@gmail.com>
2018-01-08 19:17:42 -0500

    funcionalidad de post

--- js/app.js
+++ js/app.js
@@ -8,4 +8,48 @@ $('document').ready(function() {
   $('#btnSignIn').click(function() {
     window.location.href = 'userConfig.html';
   });
+  /* funcionalidad a los post del usuario*/
+  var textArea = $('#inputPost');
+  var boton = $('#post');
+  boton.disabled = true;
+  /* Al hacer clic agregar debajo */
+  boton.click(function() {
+    /* alert('me diste clic'); */
+    var valueText = textArea.val();
+    var newDiv = $('<div class="newDiv"></div>');
+    newDiv.text(time() + ' ' + valueText);
+    var post = $('#insert-post');
+    post.append(newDiv);
+    clean();
+  });
+  /* funcion para limpiar el textArea */
+  function clean() {
+    textArea.val('');
+  }
+  /* evento para el autosize */
+  textArea.keydown(function() {
+    function autosize() {
+      var now = this;
+      setTimeout(function() {
+        now.style.cssText = 'height:auto; padding:0';
+        now.style.cssText = 'height:' + now.scrollHeight + 'px';
+      },0);
+    }
+  });
+  /* funcion para la hora del post */
+  function time() {
+    var date = new Date();
+    var hour = date.getHours();
+    var min = date.getMinutes();
+    var time;
+    if (min < 10) {
+      min = '0' + min;
+    }
+    if (hour >= 12 && hour <= 24) {
+      time = hour + ':' + min + ' PM';
+    } else {
+      time = hour + ':' + min + ' AM';
+    }
+    return time;
+  }
 });
